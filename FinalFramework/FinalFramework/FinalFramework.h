2f4e235e34e0cc7009e501b1b06e4ae7965b056e
Nirmal <nirmal.patel@canopusinfosystems.com>
2023-03-15 19:16:49 +0530

    initial commit

---
+++ FinalFramework/FinalFramework/FinalFramework.h
@@ -0,0 +1,18 @@
+//
+//  FinalFramework.h
+//  FinalFramework
+//
+//  Created by IOS on 15/03/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FinalFramework.
+FOUNDATION_EXPORT double FinalFrameworkVersionNumber;
+
+//! Project version string for FinalFramework.
+FOUNDATION_EXPORT const unsigned char FinalFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FinalFramework/PublicHeader.h>
+
+
---
+++ FinalFramework/Service.swift
@@ -0,0 +1,16 @@
+//
+//  Service.swift
+//  FinalFramework
+//
+//  Created by IOS on 15/03/23.
+//
+
+import Foundation
+
+public class Service {
+    private init() {}
+    
+    public static func doSomething() -> String {
+        return "Framework works"
+    }
+}
